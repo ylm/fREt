1943b4ed13f4849728f3ac3c852b72ab5120e63b
ylm <yan.lamarre@gmail.com>
2015-10-17 20:08:45 -0400

    Main file program... Lot of clean up to be done

---
+++ fREt.py
@@ -0,0 +1,105 @@
+#!/usr/bin/python3
+
+# Copyright (c) 2015 Yannick Lamarre
+# All rights reserved.
+
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met:
+
+# Redistributions of source code must retain the above copyright notice,
+# this list of conditions and the following disclaimer.
+
+# Redistributions in binary form must reproduce the above copyright
+# notice, this list of conditions and the following disclaimer in the
+# documentation and/or other materials provided with the distribution.
+
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+# A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+# HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+__author__ = 'Yannick Lamarre'
+__license__ = 'BSD License'
+
+import sys
+import os
+import io
+import argparse
+import curses
+import xdlrc_parser
+
+def clip_cursor(curpos_tup, maxyx_tup, ystep, xstep):
+    retval = [0, 0]
+    retval[0] = curpos_tup[0] + ystep*2
+    retval[1] = curpos_tup[1] + xstep*2
+    if retval[0] < 0:
+        retval[0] = maxyx[0] - retval[0] + 2
+    elif retval[0] > maxyx[0]:
+        retval[0] = retval[0] - maxyx[0] - 2
+    if retval[1] < 0:
+        retval[1] = maxyx[1] - retval[1] + 2
+    elif retval[1] > maxyx[1]:
+        retval[1] = retval[1] - maxyx[1] - 2
+    return retval
+
+
+def main():
+    parser = argparse.ArgumentParser(description='fpga Reverse Engineering tool')
+    parser.add_argument('file', nargs='?',
+                       help='File to edit')
+    args = parser.parse_args()
+    device_layout = load_xdlrc(args.file)
+    curses.wrapper(main_window, device_layout)
+    
+    
+def main_window(stdscr, device_layout):
+    tilesym_dict = {}
+    stdscr.clear()
+    cursor_posx = 0
+    cursor_posy = 0
+    wrkpad = curses.newpad(device_layout.tile_rows*2 - 1, device_layout.tile_cols*2 - 1)
+    for irow in range(0, device_layout.tile_rows):
+        for icol in range(0, device_layout.tile_cols):
+            itile = device_layout.tile_list[irow*device_layout.tile_cols + icol]
+            wrkpad.addstr(irow*2, icol*2, itile[4][0]+' ')
+    while True:
+        wrkpad.refresh()
+
+        if c_key == 'q':
+            break
+        elif c_key == 'h':
+        elif c_key == 'j':
+        elif c_key == 'k':
+        elif c_key == 'l':
+        elif c_key == 'y':
+        elif c_key == 'u':
+        elif c_key == 'b':
+        elif c_key == 'n':
+        else:
+            pass
+
+
+def manage_tile_dictionary(device_layout):
+    """Manages the dictionary for screen's tile symbols
+
+    The dictionary mapping a character to be displayed to a tile name
+    I
+
+    Args:
+        device_layout: A DeviceLayout object containing device information
+
+    Returns:
+        A dict mapping a tile name to a character to be displayed
+
+    """
+    return_value = []
+
+main()
